355c4e869bf0f4c7913ce84042ed999bf42d0c79
Arshia <arshiahsn@gmail.com>
2021-01-07 23:39:33 -0700

    token logs

--- server/index.js
+++ server/index.js
@@ -9,8 +9,10 @@ passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-    }, () => {
-        console.log(accessToken);
+    }, (accessToekn, refreshToken, profile, done) => {
+        console.log('access token', accessToken);
+        console.log('refresh token', refreshToken);
+        console.log('profile', profile);
     })
 );
 
