9670c648add43c698994ff5ffde2bea42fcdf2e7
Arshia <arshiahsn@gmail.com>
2021-01-07 23:14:48 -0700

    callback working

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = requre('./config/keys');
+const keys = require('./config/keys');
 
 const app = express();
 
@@ -15,11 +15,18 @@ passport.use(new GoogleStrategy({
 );
 
 app.get('/auth/google', 
-    passport.authenticate('google',{
+    passport.authenticate('google', {
         scope: ['profile','email']
     })
+);
+
+app.get('/auth/google/callback',
+    passport.authenticate('google')
+
 );
 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+
