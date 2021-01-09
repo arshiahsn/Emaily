6037b7c6063809fb2280b5400c815debffd445cd
Arshia <arshiahsn@gmail.com>
2021-01-08 23:57:37 -0700

    refactor

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 require('./services/passport.js');
-const authRoutes = require('./routes/authRoutes');
-const app = express();
 
-authRoutes(app);
+const app = express();
+require('./routes/authRoutes')(app);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,16 +3,13 @@ const passport = require('passport');
 
 module.exports = () => {
     app.get('/auth/google', 
-    passport.authenticate('google', {
-        scope: ['profile','email']
-    })
+        passport.authenticate('google', {
+            scope: ['profile','email']
+        })
     );
 
     app.get('/auth/google/callback',
-    passport.authenticate('google')
-
+        passport.authenticate('google')
     );
-
-
 
 };
