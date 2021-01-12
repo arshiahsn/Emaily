24d43aec11cdff52301ae5c4a19cc9ac869fc30f
Arshiahsn <arshiahsn@gmail.com>
2021-01-12 00:28:26 -0700

    keys fix

--- server/config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    googleClientID: '1036918324009-07gff3e7cur2u6i6um93rmlddhvl8voe.apps.googleusercontent.com',
-    googleClientSecret: 'bVgbLATEfXzlLqowq-AhpzJ9',
-    mongoURI: 'mongodb+srv://arshiahsn:iEltMewRRV1p4fp8@cluster0.8wech.mongodb.net/emaily?retryWrites=true&w=majority',
-    cookieKey: 'redacted',
-};
