7bab51d86d36a81ea1c72c6c96b94e75c288d8d8
Arshia <arshiahsn@gmail.com>
2021-01-26 22:29:13 -0700

    added cookie session

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
+
+
 require('./models/User');
 require('./services/passport');
 
@@ -8,6 +12,15 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+
+    
+);
 
 
 require('./routes/authRoutes')(app);
