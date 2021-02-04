c66231d3866e4b068e3ca32afd737c059a610335
Arshia <arshiahsn@gmail.com>
2021-02-03 22:00:00 -0700

    react client added

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi, it's Arshia here!
         </p>
         <a
           className="App-link"
