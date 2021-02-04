2aea859b0a9855496c874b81d19cd985133fa3f5
Arshia <arshiahsn@gmail.com>
2021-02-03 21:56:37 -0700

    removed extra dir

--- server/config/dev.js
+++
@@ -1,7 +0,0 @@
-//dev.js
-module.exports = {
-    googleClientID: '1036918324009-07gff3e7cur2u6i6um93rmlddhvl8voe.apps.googleusercontent.com',
-    googleClientSecret: 'bVgbLATEfXzlLqowq-AhpzJ9',
-    mongoURI: 'mongodb+srv://arshiahsn:iEltMewRRV1p4fp8@cluster0.8wech.mongodb.net/emaily?retryWrites=true&w=majority',
-    cookieKey: 'asndoansdjnasjdn',
-};
