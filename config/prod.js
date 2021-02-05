3fedac9ee6767370742ec2255728cbd5cf4b8d70
Arshia <arshiahsn@gmail.com>
2021-02-04 19:08:31 -0700

    fixed keys

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+//prod.js
+module.exports = {
+    googleClientID: '76121124692-h2va3ddnp3ft8pfosnltdbublr3ufmad.apps.googleusercontent.com',
+    googleClientSecret: '-yut8t0SInwgmR3CpEVdK-nK',
+    mongoURI: 'mongodb+srv://emaily-prod:wDh1l0J2D3T8T147@cluster0.aqy8h.mongodb.net/emaily-prod?retryWrites=true&w=majority',
+    cookieKey: 'kjnuqekjbdnkahjsbdhkjabs',
+};
