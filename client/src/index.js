7b0cce0cbeac99ffc6283ff9d57695ed4b9154ed
Arshiahsn <arshiahsn@gmail.com>
2021-03-04 18:42:05 -0700

    reducer index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import {createStore, applyMiddleware} from 'redux';
 
 
 import App from './components/App';
+import reducers from './reducers';
 
 const store = createStore(()=>[],{},applyMiddleware());
 
