bf498731ad22f0175ded8df34289c30053e8ec6a
Arshiahsn <arshiahsn@gmail.com>
2021-05-18 17:23:40 -0600

    added stripe keys

--- server/config/dev.js
+++ server/config/dev.js
@@ -4,4 +4,6 @@ module.exports = {
     googleClientSecret: 'bVgbLATEfXzlLqowq-AhpzJ9',
     mongoURI: 'mongodb+srv://arshiahsn:iEltMewRRV1p4fp8@cluster0.8wech.mongodb.net/emaily?retryWrites=true&w=majority',
     cookieKey: 'asndoansdjnasjdn',
+    stripePublishableKey: 'pk_test_51IsbDhCSThGaZFmvGj4lDJAj6dyZJO4U0Bc8lVwgdWCknUSdWw8LLqIbWmO8434dYROodvWmsQucgAAb97yCtzdB00cET6ufrr',
+    stripeSecretKey: 'sk_test_51IsbDhCSThGaZFmvkV63YKMZfwmhd1V2TG0PB7wdwYsUcG4idHE9axCtawszZ9MZY94BnNcTQEWIWED99QtUyzhR0004LMqHuL'
 };
